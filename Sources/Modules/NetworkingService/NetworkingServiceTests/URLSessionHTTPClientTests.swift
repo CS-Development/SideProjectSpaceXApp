814d2602854a8b82cf4343d36e2fd6398ca4d932
Christian Slanzi <christian.slanzi@gmail.com>
2021-10-26 17:09:41 +0200

    create a URLSessionHTTPClient's unit test class

---
+++ Sources/Modules/NetworkingService/NetworkingServiceTests/URLSessionHTTPClientTests.swift
@@ -0,0 +1,20 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  NetworkingServiceTests
+//
+//  Created by Christian Slanzi on 26.10.21.
+//
+
+import XCTest
+
+class URLSessionHTTPClientTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
