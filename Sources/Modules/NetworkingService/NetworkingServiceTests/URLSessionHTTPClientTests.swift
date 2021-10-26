07674d7b485fa080a95ed5ed5b48c136041c5687
Christian Slanzi <christian.slanzi@gmail.com>
2021-10-26 19:30:39 +0200

    implemented  the URLProtocolStub

--- Sources/Modules/NetworkingService/NetworkingServiceTests/URLSessionHTTPClientTests.swift
+++ Sources/Modules/NetworkingService/NetworkingServiceTests/URLSessionHTTPClientTests.swift
@@ -28,6 +28,56 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     private class URLProtocolStub: URLProtocol {
         
+        private static var stub: Stub?
+        private static var requestObserver: ((URLRequest) -> Void)?
+
+        // the HTTPResult Stub
+        
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+        }
+        
+        // MARK: override URLProtocol's methods
+
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+
+        override func startLoading() {
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+            if let data = URLProtocolStub.stub?.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            if let response = URLProtocolStub.stub?.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            if let error = URLProtocolStub.stub?.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() {}
+        
+        // MARK: static helper methods for the stubbing/intercepting
+
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
+            stub = Stub(data: data, response: response, error: error)
+        }
+        
         static func startInterceptingRequests() {
             URLProtocolStub.registerClass(URLProtocolStub.self)
         }
